104c02a7eab474a3348a89a84a87e26f517e6159
JustNao <nao.3nicolas@hotmail.fr>
2023-06-23 22:54:18 +0200

    Updated server adress

--- src/sniffer/check.py
+++ src/sniffer/check.py
@@ -10,7 +10,7 @@ def check_for_update():
         current_saved_version = f.read().strip()
 
     cloud_version = rq.get(
-        "https://9abb-2a01-cb00-dcf-1000-8d02-49f5-10ba-303b.ngrok-free.app/DOFUS_VERSION"
+        "https://b37a-2a01-cb00-dcf-1000-a6d1-91af-b3c4-87ba.ngrok-free.app/DOFUS_VERSION"
     )
     if not cloud_version.ok:
         print("Error while fetching version from server, auto-update disabled.")
@@ -21,7 +21,7 @@ def check_for_update():
     if cloud_version != current_saved_version:
         print("New version found, updating...")
         packets = rq.get(
-            "https://9abb-2a01-cb00-dcf-1000-8d02-49f5-10ba-303b.ngrok-free.app/protocol.pk",
+            "https://b37a-2a01-cb00-dcf-1000-a6d1-91af-b3c4-87ba.ngrok-free.app/protocol.pk",
         )
         with open(protocol_path, "wb") as f:
             f.write(packets.content)
