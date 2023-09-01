4c51e1b16ed83c3964067e11ab19493dcf403b05
JustNao <nao.3nicolas@hotmail.fr>
2023-09-01 17:10:00 +0200

    Changed auto update to github servers

--- src/sniffer/check.py
+++ src/sniffer/check.py
@@ -10,7 +10,7 @@ def check_for_update():
         current_saved_version = f.read().strip()
 
     cloud_version = rq.get(
-        "https://b37a-2a01-cb00-dcf-1000-a6d1-91af-b3c4-87ba.ngrok-free.app/DOFUS_VERSION"
+        "https://raw.githubusercontent.com/JustNao/Karrelage/master/DOFUS_VERSION"
     )
     if not cloud_version.ok:
         print("Error while fetching version from server, auto-update disabled.")
@@ -21,9 +21,13 @@ def check_for_update():
     if cloud_version != current_saved_version:
         print("New version found, updating...")
         packets = rq.get(
-            "https://b37a-2a01-cb00-dcf-1000-a6d1-91af-b3c4-87ba.ngrok-free.app/protocol.pk",
+            "https://raw.githubusercontent.com/JustNao/Karrelage/master/src/sniffer/protocol.pk",
         )
         with open(protocol_path, "wb") as f:
             f.write(packets.content)
         with open("DOFUS_VERSION", "w") as f:
             f.write(cloud_version)
+        print(f"Updated to {cloud_version}")
+        return
+
+    print("No update found")
