bb516d514d90bfb592706d589a2651fb2dea7dfe
JustNao <nao.3nicolas@hotmail.fr>
2023-11-12 21:12:19 +0100

    Continued treasure hunter

--- src/entities/maps.py
+++ src/entities/maps.py
@@ -1,14 +1,14 @@
 from .utils import load
 
 mapPositions = load("MapPositions")
-mapToPositions_dict = {map["id"]: (map["posX"], map["posY"]) for map in mapPositions}
+mapToPositions_dict = {map["id"]: ([map["posX"], map["posY"]]) for map in mapPositions}
 del mapPositions
 
 
-def get_map_positions(map_id: int) -> tuple[int, int]:
+def get_map_positions(map_id: int) -> list[int]:
     """Returns the position of a map from its id"""
     try:
         return mapToPositions_dict[map_id]
     except KeyError:
         print("Couldn't identify", map_id)
-        return 0, 0
+        return [0, 0]
--- src/modules/treasure_hunter.py
+++ src/modules/treasure_hunter.py
@@ -1,44 +1,44 @@
 import json
-from threading import Thread
 import time
 import pyperclip
 import pygetwindow
 import pyautogui as ag
+from seleniumwire import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from seleniumwire.utils import decode
 from src.entities.id import get_monster_name, get_poi_name
 from src.entities.maps import get_map_positions
 from src.entities.utils import load
-from src.utils.qt import ConfigUI
+from src.utils.data import load_dat
 from src.modules.base import DofusModule
+
 
 class TreasureHunter(DofusModule):
     def __init__(self):
         # Running selenium webdriver in the background to simulate the user's clicks on the site
-        # options = Options()
-        # # options.add_argument('--headless')
-        # options.add_argument("log-level=1")
-        # self.driver = webdriver.Chrome(chrome_options=options)
-        # self.driver.get("https://dofusdb.fr/fr/tools/treasure-hunt")
+        options = Options()
+        # options.add_argument('--headless')
+        options.add_argument("log-level=1")
+        self.driver = webdriver.Chrome(options=options)
+        self.driver.get("https://dofusdb.fr/fr/tools/treasure-hunt")
 
         self.player_position = self.Position()
         self.hint_position = self.Hint()
-        self.timeStart = None
-        self.timeStart = 0
+        self.time_start = 0.0
         self.direction = "stay"
-        self.clientHintName = None
         self.check_positions = []
         self.phorreur = {"lookingFor": False, "npcId": 2673}
         self.autopilot = False
-        self.autopilotMoving = False
+        self.autopilot_moving = False
         self.relevant_monsters = load("Archi")
-        # dofus_windows = pygetwindow.getWindowsWithTitle("- Dofus")
-        # if len(dofus_windows) > 0:
-        #     self.dofus_window = dofus_windows[0]
-        # else:
-        #     raise Exception("Dofus needs to be launched before starting the bot")
-        self.setup_ui()
+        dofus_windows = pygetwindow.getWindowsWithTitle("- Dofus")
+        if len(dofus_windows) > 0:
+            self.dofus_window = dofus_windows[0]
+        else:
+            raise Exception("Dofus needs to be launched before starting the bot")
+        self.setup_bot()
 
     class Hint:
         def __init__(self, posX=0, posY=0, x_d=666, y_d=666):
@@ -74,8 +74,11 @@ class TreasureHunter(DofusModule):
                 and obj.y == self.y
             )
 
-    def exit(self):
-        self.driver.quit()
+    def is_hint_too_far(self):
+        return self.hint_position.x_distance > 10 or self.hint_position.y_distance > 10
+
+    # def exit(self):
+    #     self.driver.quit()
 
     def move(self):
         # Déplacement du bot
@@ -83,9 +86,7 @@ class TreasureHunter(DofusModule):
         if self.direction == "stay":
             return
 
-        if (not self.botting) or (
-            self.hint_position.distance > 10 and not self.phorreur["lookingFor"]
-        ):
+        if self.is_hint_too_far() and not self.phorreur["lookingFor"]:
             return
 
         # For first step, the hint will likely be away
@@ -96,11 +97,11 @@ class TreasureHunter(DofusModule):
             return
 
         # If the autopilot is on, we don't move the bot
-        if self.autopilotMoving:
+        if self.autopilot_moving:
             return
 
         if not self.phorreur["lookingFor"]:
-            self.autopilotMoving = True
+            self.autopilot_moving = True
             x, y = self.hint_position.x, self.hint_position.y
             ag.hotkey("space")
             print("Autopilot : moving to", x, y)
@@ -116,13 +117,26 @@ class TreasureHunter(DofusModule):
     def next_step(self):
         pass
 
-    def setup_ui(self):
-        self.config_ui = ConfigUI()
-        self.config_positions = self.config_ui.start_loop()
+    def setup_bot(self):
+        screen_x, screen_y = ag.size()
+        ui_positions = load_dat("Berilia_ui_positions")
+        try:
+            middle_banner = ui_positions["banner##pos##mainCtr##default"]
+        except KeyError:
+            raise Exception("Middle banner not found")
+        self.move_positions = {
+            "right": (screen_x - 50, screen_y // 2),
+            "left": (50, screen_y // 2),
+            "top": (screen_x // 2, 10),
+            "bottom": (
+                (middle_banner["x"] + 270.2) * 1.404,
+                middle_banner["y"] * 1.404,
+            ),  # TODO: check if this is the right ratio for all resolutions
+        }
 
     def handle_CurrentMapMessage(self, packet):
         """Triggered when the player changes map"""
-        self.player_position = self.Position(*get_map_positions[packet["mapId"]])
+        self.player_position = self.Position(*get_map_positions(packet["mapId"]))
         self.step_update()
 
     def handle_TreasureHuntMessage(self, packet):
@@ -132,9 +146,9 @@ class TreasureHunter(DofusModule):
         if len(packet["flags"]) == 0:
             # First step of the hunt
             self.check_positions.clear()
-            self.check_positions.append(get_map_positions[packet["startMapId"]])
+            self.check_positions.append(get_map_positions(packet["startMapId"]))
             if packet["checkPointCurrent"] == 0:
-                self.timeStart = time.time()
+                self.time_start = time.time()
                 self.player_position = self.Position(-25, -36)  # Malle aux trésors
 
         elif len(packet["flags"]) == packet["totalStepCount"]:
@@ -145,11 +159,7 @@ class TreasureHunter(DofusModule):
         if packet["checkPointCurrent"] >= packet["checkPointTotal"] - 1:
             # Last step of the hunt
             end = time.time()
-            print(
-                "This hunt took ",
-                str(int(end - self.timeStart)),
-                " seconds to finish",
-            )
+            print(f"This hunt took {int(end - self.time_start)} seconds to finish")
             return
 
         direction_int = packet["knownStepsList"][-1]["direction"]
@@ -166,21 +176,21 @@ class TreasureHunter(DofusModule):
             packet["knownStepsList"][-1]["__type__"]
             == "TreasureHuntStepFollowDirectionToPOI"
         ):
+            # Decor element to look for
             client_hint_name = get_poi_name(packet["knownStepsList"][-1]["poiLabelId"])
             self.get_hint(packet, client_hint_name)
-            if self.hint_position.distance == 666:
+            if self.hint_position.x_distance == 666:
                 print("ERROR : no hint found !")
                 self.direction = "stay"
             else:
                 self.check_positions.append(
                     (self.hint_position.x, self.hint_position.y)
                 )
-                print(
-                    f"{client_hint_name} found {self.hint_position.distance} maps at {self.hint_position.__str__()}"
-                )
+                print("Hint found !")
                 self.step_update()
                 self.move()
         else:
+            # Phorreur to look for
             self.phorreur["lookingFor"] = True
             self.phorreur["npcId"] = packet["knownStepsList"][-1]["npcId"]
             if (len(packet["flags"]) == 0) and (packet["checkPointCurrent"] == 0):
@@ -209,7 +219,7 @@ class TreasureHunter(DofusModule):
                 if get_monster_name(main_monster) in self.relevant_monsters:
                     print("Archimonstre found !")
                     self.direction = "stay"
-                    self.autopilotMoving = False
+                    self.autopilot_moving = False
                     # TODO: add a stop for the autopilot
                     break
         self.move()
@@ -239,16 +249,17 @@ class TreasureHunter(DofusModule):
         if self.player_position == self.hint_position:
             print("Hint found !")
             self.direction = "stay"
-            self.autopilotMoving = False
+            self.autopilot_moving = False
             time.sleep(1)
             # TODO: add a button to click to go to the next step
             self.next_step()
 
     def reset(self):
-        self.hint_position.distance = 666
+        self.hint_position.x_distance = 666
+        self.hint_position.y_distance = 666
         self.direction = "stay"
         self.phorreur["lookingFor"] = False
-        self.autopilotMoving = False
+        self.autopilot_moving = False
 
     def get_DofusDB_pos(self, hintBody, poiToLookFor):
         hintBody["data"].sort(key=lambda x: x["distance"])
@@ -283,6 +294,8 @@ class TreasureHunter(DofusModule):
             return self.get_DofusDB_request(posX, posY, direction, poiToLookFor)
 
         response = hintRequest.response
+        if not response:
+            raise Exception("No response from DofusDB")
         body = decode(
             response.body, response.headers.get("Content-Encoding", "identity")
         )
@@ -303,7 +316,7 @@ class TreasureHunter(DofusModule):
         else:
             lastCheckPoint = packet["flags"][-1]["mapId"]
 
-        posX, posY = get_map_positions[lastCheckPoint]
+        posX, posY = get_map_positions(lastCheckPoint)
         direction = direction_for_DofusDB[self.direction]
 
         newPosX, newPosY, distance = self.get_DofusDB_request(
